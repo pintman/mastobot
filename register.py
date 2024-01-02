1a2be18c23dbb58ee975860cf8ef3fcdbc416608
Marco Bakera <marco@bakera.de>
2024-01-02 10:30:04 +0100

    doc

--- register.py
+++ register.py
@@ -1,6 +1,11 @@
 from mastodon import Mastodon
 import config
 
+# Register your app! This only needs to be done once (per server, or when
+# distributing rather than hosting an application, most likely per device and server).
+# Uncomment the code and substitute in your information:
+# https://github.com/halcy/Mastodon.py
+#
 Mastodon.create_app(
      'toot_bot',
      api_base_url = config.API_BASE_URL,
